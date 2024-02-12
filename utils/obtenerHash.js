77777d8e7b3e405bf0795d84a453db7666a19e3b
daniel caamal <danielcaamal97@gmail.com>
2024-02-12 12:27:03 -0600

    FTR: Undo changes from empresarial

--- utils/obtenerHash.js
+++ utils/obtenerHash.js
@@ -2,11 +2,10 @@
 exports.__esModule = true;
 exports.obtenerHash = void 0;
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 var crypto_js_1 = require("crypto-js");
 var obtenerHash = function (nss, userToken) {
     var sesionData = userToken !== null ? userToken : nss;
-    var sesionHash = crypto_js_1["default"].SHA512(sesionData).toString();
+    var sesionHash = (0, crypto_js_1.SHA512)(sesionData).toString();
     if (!sesionHash) {
         throw new Error("No se ha podido generar el hash de la sesion");
     }
--- utils/obtenerHash.ts
+++ utils/obtenerHash.ts
@@ -1,10 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import crypto from "crypto-js";
+import { SHA512 } from "crypto-js";
 
 export const obtenerHash = (nss: string, userToken?: string): string => {
   const sesionData = userToken !== null ? userToken : nss;
-  const sesionHash = crypto.SHA512(sesionData as string).toString();
+  const sesionHash = SHA512(sesionData as string).toString();
 
   if (!sesionHash) {
     throw new Error("No se ha podido generar el hash de la sesion");
