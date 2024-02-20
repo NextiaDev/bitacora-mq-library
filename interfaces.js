7a8483e8fa43b9a68ed9a871307c4aa59763004f
daniel caamal <danielcaamal97@gmail.com>
2024-02-20 17:20:20 -0600

    FTR: Resolve conflicts

--- interfaces.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.BITACORA_TYPES = void 0;
-exports.BITACORA_TYPES = {
-    1: "read",
-    2: "update",
-    3: "instert",
-    4: "delete"
-};
---
+++ test/example.ts
@@ -0,0 +1,47 @@
+import { registrar } from "../src/index";
+
+
+const exampleToCopy = async () => {
+  const response = await registrar(1, {
+    bitacoraBody: {
+      IP: "localhost",
+      nss: "1234567890", // IE: "1234567890"
+      origen: "MCI",
+      responseCode: 200,
+      valorAnterior: "",
+      valorNuevo: "",
+      geolocalizacion: "",
+      request: "",
+      response: {
+        prueba: true,
+      },
+      token: null,
+    },
+    bitacoraOptions: {
+      method: "",
+      protocol: "https",
+      hostname: "",
+      port: "",
+      path: "",
+      timeout: "7000",
+      keyId: "",
+    },
+    tokenOptions: {
+      method: "",
+      protocol: "https",
+      hostname: "",
+      port: "",
+      path: "",
+      timeout: "7000",
+      keyId: "",
+      usuario: "",
+      contrasena: "",
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+  console.log(response);
+};
+
+exampleToCopy();
